7795bce72a96cb76b3732f0ab882aee4a7d79ee5
Abhir-24 <abhir.raj2410@gmail.com>
2021-10-04 14:29:15 +0530

    fixed login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 // import Home from './components/Home';
-import Login from './components/Login/Login';
+import Login from './components/Login/Login2';
 import Register from './components/Register/Register';
 
 const App = () => {
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,50 +1,152 @@
-import React from 'react';
-import Avatar from '../../images/avatar.png';
-import { Link } from 'react-router-dom';
-import '../Register/Register.css';
+import React, {Component} from 'react';
+import { Redirect } from 'react-router';
+import './Login.css';
 
-const Login = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
+
+class Login extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      details: {},
+      errors: {},       
+      redirect: false
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+
   };
+
+  handleChange(e) {
+    let details = this.state.details;
+    details[e.target.name] = e.target.value;
+    this.setState({
+      details
+    });
+
+  }
+
+  submitForm(e) {
+
+    e.preventDefault();
+
+    if (this.validateForm()) {
+        let details = {};
+        details["username"] = "";
+        details["emailid"] = "";
+        details["password"] = "";
+        this.setState({details:details});
+        alert("You have been logged in successfully!"); 
+        this.setState({redirect: true});
+      }
+
+  }
+
+  validateForm() {
+
+    let details = this.state.details;
+    let errors = {};
+    let validity = true;
+
+    if (!details["username"]) {
+      validity = false;
+      errors["username"] = "*Please enter your Username";
+    }
+
+    if (typeof details["username"] !== "undefined") {
+      if (!(details["username"].length > 5)) {
+        validity = false;
+        errors["username"] = "*Please enter more than 5 characters";
+      }
+    }
+
+    if (!details["emailid"]) {
+      validity = false;
+      errors["emailid"] = "*Please enter your Email ID";
+    }
+
+    if (typeof details["emailid"] !== "undefined") {
+
+      var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+      if (!pattern.test(details["emailid"])) {
+        validity = false;
+        errors["emailid"] = "*Please enter valid Email ID";
+      }
+    }
+
+    if (!details["password"]) {
+      validity = false;
+      errors["password"] = "*Please enter your Password";
+    }
+
+    if (typeof details["password"] !== "undefined") {
+      if (!(details["password"].length > 6)) {
+        validity = false;
+        errors["password"] = "*Please enter more than 6 characters";
+      }
+    }
+
+    this.setState({
+      errors: errors
+    });
+    return validity;
+
+  }
+
+
+render() {
   return (
-    <section className="Register-Form">
-      <form>
-        <div className="box">
-          <div className="Avt">
-            <img src={Avatar} alt="" />
-          </div>
-          <div className="RegF-Heading">
-            <h4>Login</h4>
-          </div>
-          <div className="form-fields">
-            <div className="input-box">
-              <input
-                type="text"
-                placeholder="Enter your Username or Email"
-                className="form-control"
-              />
+
+   <div className="login">
+
+     <div className="container">
+
+        <form className="form" method="post" name="RegistrationForm" onSubmit= {this.submitForm}>
+
+            <h2>SignUp To Register</h2>
+            
+            <div className="control">
+                <label htmlFor = "username">Username</label>
+                <input type="text" 
+                name = "username" 
+                value={this.state.details.username} 
+                placeholder="Username" 
+                onChange={this.handleChange} />
+                <small className="errorMsg">{this.state.errors.username}</small>
             </div>
-            <div className="input-box">
-              <input
-                type="Password"
-                placeholder="Enter your Password"
-                className="form-control"
-              />
-              <div className="button-box">
-                <button type="submit" onClick={handleSubmit}>
-                  Login
-                </button>
-                <Link to="/register">
-                  <span>New? Create account.</span>
-                </Link>
-              </div>
+
+            <div className="control">
+                <label htmlFor = "email">Email</label>
+                <input type="text" 
+                name = "emailid" 
+                value={this.state.details.emailid}  
+                placeholder="Enter Email" 
+                onChange={this.handleChange} />
+                <small className="errorMsg">{this.state.errors.emailid}</small>
             </div>
-          </div>
-        </div>
-      </form>
-    </section>
-  );
-};
+
+            <div className="control">
+                <label htmlFor = "password">Password</label>
+                <input type="password" 
+                name = "password" 
+                value={this.state.details.password} 
+                placeholder="Enter Password" 
+                onChange={this.handleChange} />
+                <small className="errorMsg">{this.state.errors.password}</small>
+            </div>
+
+            <input type="submit" className="button"  value="Register" />
+        </form>
+        {this.state.redirect ? <Redirect to="/register" /> : ''}
+    </div>
+  </div>
+
+    );
+}
+
+}
+
 
 export default Login;
